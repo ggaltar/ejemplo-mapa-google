46b41b1885485c2c319534139c881bdf036d4c0c
Manuel Vargas <mfvargas@gmail.com>
2021-06-22 20:58:06 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,6 @@ function initMap() {
   mapa = new google.maps.Map(document.getElementById("mapa"), {
     center: { lat: 10.0, lng: -84.0 },
     zoom: 8,
+    mapTypeId: 'hybrid' // roadmap, satellite, hybrid, terrain
   });
 }
